e42aa9f10d1a371142eb135813b213147a473168
Sugam Raut <sugamrautbim@gmail.com>
2025-05-01 19:04:20 +0545

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:'class',
   content: ['*.{html,js}'],
   theme: {
     extend: {
       fontFamily : {
         domine : ['Domine','serif']
+      },
+      animation:{
+        'rotating':"rotating 5s linear infinite"
+      },
+      keyframes:{
+        rotating:{
+          '0%':{transform:'rotate(0.0deg)'},
+          '25%':{transform:'rotate(90deg)'},
+          '50%':{transform:'rotate(180deg)'},
+          '75%':{transform:'rotate(270deg)'},
+          '100%':{transform:'rotate(360deg)'},
+        }
       }
     },
   },
